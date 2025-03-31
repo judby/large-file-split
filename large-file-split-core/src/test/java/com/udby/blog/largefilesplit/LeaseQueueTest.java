b25b75b4cbc29f94efdd51d8bc7d6d88f2643e59
Jesper Udby <jesper@udby.com>
2025-03-31 17:03:45 +0200

    move license

--- large-file-split-core/src/test/java/com/udby/blog/largefilesplit/LeaseQueueTest.java
+++ large-file-split-core/src/test/java/com/udby/blog/largefilesplit/LeaseQueueTest.java
@@ -1,5 +1,3 @@
-package com.udby.blog.largefilesplit;
-
 /*
 Copyright 2025 Jesper Udby
 
@@ -15,6 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+package com.udby.blog.largefilesplit;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
